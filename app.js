e2176f72bfb17700e4b3026a927df0d9a8fc95bc
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2023-03-30 19:47:01 +0530

    Added some custom and third party middleware

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("Hello from server !!");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, "utf-8")
@@ -12,6 +23,7 @@ const tours = JSON.parse(
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
+    requestedAt: req.requestTime,
     data: {
       tours,
     },
