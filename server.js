8c0308d60fa8d18719b817fdb9517d474dd0e843
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2023-04-01 11:27:52 +0530

    Added DB connection and tour model

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./.env" });
 const app = require("./app");
 
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  encodeURIComponent(process.env.DATABASE_PASSWORD)
+);
+// console.log(DB);
+mongoose.connect(DB).then((con) => {
+  console.log("Connection successfull !!");
+  console.log(con.connections);
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name."],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price."],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 // console.log(app.get("env"));
 // console.log(process.env);
 
