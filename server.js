0637a6a643c66f3db16f4df2a38b47c0f5a57772
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2023-04-01 12:03:03 +0530

    Added test tour

--- server.js
+++ server.js
@@ -32,6 +32,19 @@ const tourSchema = new mongoose.Schema({
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Park Camper",
+  price: 997,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
 // console.log(app.get("env"));
 // console.log(process.env);
 
