d8c49d5adb54d4f63cc619f343a6946bdec21522
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2023-04-02 22:50:31 +0530

    added standard filters in api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -17,9 +17,17 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((ele) => delete queryObj[ele]);
+
+    const query = Tour.find(queryObj);
+
+    const tours = await query;
+
     res.status(200).json({
       status: "success",
+      results: tours.length,
       data: {
         tours,
       },
