1b9f811c25bbdb0a641d5b7b2ebd043872024df1
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2023-04-10 09:10:34 +0530

    Added tour stats and monthly tours using aggregation pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -13,6 +13,8 @@ router.use((req, res, next) => {
 router
   .route("/top-5-best-cheap")
   .get(tourController.top5BestCheap, tourController.getAllTours);
+router.route("/tour-stats").get(tourController.getTourStats);
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 router
   .route("/")
   .get(tourController.getAllTours)
